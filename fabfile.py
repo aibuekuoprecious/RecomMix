eb4127dbf5a42ce31c837a84670c5597690396f7
Precious Okwuoma Aibueku <aibuekuprecious@gmail.com>
2023-12-07 13:11:57 +0000

    deployment

--- fabfile.py
+++ fabfile.py
@@ -1,8 +1,10 @@
 # fabfile.py
 
+import os
 from fabric import task, Connection
 
 # Define the paths
+PRIVATE_KEY_PATH = os.path.expanduser("~/.ssh/school.pem")
 BASE_DIR = "/RecomMix"
 APP_DIR = f"{BASE_DIR}/app"
 
@@ -11,8 +13,8 @@ GUNICORN_SERVICE = "recommix"
 
 @task
 def deploy(ctx):
-    # Connect to the server
-    with Connection("root@92fcd47de30b") as conn:
+    # Create a new connection inside the task
+    with Connection("ubuntu@34.224.63.159", connect_kwargs={"key_filename": PRIVATE_KEY_PATH}) as conn:
         # Update the codebase from the Git repository
         conn.run(f"cd {BASE_DIR} && git pull origin main")
 
