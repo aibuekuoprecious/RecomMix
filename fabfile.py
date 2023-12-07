ff63c8867d45f9e7a874b4566d0f6e59b6f498d3
Precious Okwuoma Aibueku <aibuekuprecious@gmail.com>
2023-12-07 11:47:05 -0800

    deployment

--- fabfile.py
+++ fabfile.py
@@ -1,19 +1,28 @@
-# fabfile.py
-
 import os
 from fabric import task, Connection
 
+# Get the absolute path to the directory containing this script
+CURRENT_DIR = os.path.dirname(__file__)
+
 # Define the paths
-PRIVATE_KEY_PATH = os.path.expanduser("~/.ssh/school.pem")
-BASE_DIR = "/RecomMix"
-APP_DIR = f"{BASE_DIR}/app"
+PRIVATE_KEY_PATH = os.path.expanduser("~/.ssh/school")
+BASE_DIR = os.path.join("RecomMix")
+APP_DIR = os.path.join(BASE_DIR, "app")
 
 # Define the Gunicorn service name
 GUNICORN_SERVICE = "recommix"
 
+conn = Connection(
+    host='34.224.63.159',
+    user='ubuntu',
+    connect_kwargs={
+        'key_filename': PRIVATE_KEY_PATH
+    }
+)
+
 @task
 def deploy(ctx):
-    # Create a new connection inside the task
+    # Connect to the server
     with Connection("ubuntu@34.224.63.159", connect_kwargs={"key_filename": PRIVATE_KEY_PATH}) as conn:
         # Update the codebase from the Git repository
         conn.run(f"cd {BASE_DIR} && git pull origin main")
